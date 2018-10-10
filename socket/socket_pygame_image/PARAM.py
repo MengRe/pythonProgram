9b69322c098a7a7ecbb19776209f464a21e7aacc
AllureLover <duanshengshun@outlook.com>
2018-10-10 12:29:53 +0800

    Create PARAM.py

---
+++ socket/socket_pygame_image/PARAM.py
@@ -0,0 +1,24 @@
+# one minute to save a picture 
+TIME_TO_SAVE_PIC= 50
+
+# to identity the picture is changed
+BMP_FLAG = 0
+
+# 记录进入updated的次数，计数到达ONE_MINUTER后，将图片存到文件中并
+TIMES_ENTER_UPDATE = 0
+
+# cache 的filename
+
+# FILENAME = './cache/save.bmp'
+
+Server_addr = '192.168.43.153'
+
+
+
+
+
+# 存储字节流图片
+IMAGE_STORE = None
+
+# 图像大小
+IMAGE_SIZE = (640, 480)
